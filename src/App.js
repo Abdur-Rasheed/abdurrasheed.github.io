598eede9b80b54b0daf82d04c78f353bac32e8d9
Abdur-Rasheed <abdurrasheedsyed512@gmail.com>
2021-12-20 19:34:21 -0500

    working on project links

--- src/App.js
+++ src/App.js
@@ -1,21 +1,78 @@
 import "./App.css";
-import logo from "../src/abdurrasheed.JPG";
+import { useState } from "react";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import { Link } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
+//import photo from "./Components/abdurrasheed.JPG";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import Main from "./Components/Main";
 
 function App() {
+  const [date, setDate] = useState(new Date());
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>My name is Abdur-Rasheed Syed and i'm going to master React!</p>
-        <a
-          className="App-link"
-          href="https://github.com/Abdur-Rasheed"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check Me Out!
-        </a>
-      </header>
+      {/* <header className="App-header"> */}
+      <Layout>
+        <Header className="header" scroll>
+          <Navigation>
+            <Link to="/">Home</Link>
+
+            <Link to="/About">About</Link>
+            <Link to="/Resume">Resume</Link>
+            <Link to="/Project">Project</Link>
+            <Link to="/Contact">Contact</Link>
+          </Navigation>
+        </Header>
+        <Drawer title="Select">
+          <Navigation>
+            <Link to="/">Home</Link>
+            <Link to="/About">About</Link>
+            <Link to="/Resume">Resume</Link>
+            <Link to="/Project">Project</Link>
+            <Link to="/Contact">Contact</Link>
+          </Navigation>
+          <br />
+          <div className="Social_Links">
+            <SocialIcon url="https://mobile.twitter.com/AbdurRasheedSy3" />
+            <br />
+
+            <br />
+            <SocialIcon url="https://www.instagram.com/asyed512/" />
+            <br />
+            <SocialIcon url="https://www.linkedin.com/in/abdur-rasheed-syed/" />
+            <br />
+            <SocialIcon url="https://github.com/Abdur-Rasheed/" />
+          </div>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+
+          <Main />
+
+          <div className="calendar-container">
+            <Calendar onChange={setDate} value={date} />
+          </div>
+          <h2 className="text-center">
+            <span className="bold">Today is:</span> {date.toDateString()}
+          </h2>
+          <hr />
+          <div className="text-banner">
+            <div className="Social_Links">
+              <SocialIcon url="https://mobile.twitter.com/AbdurRasheedSy3" />
+              {"               "}
+              <SocialIcon url="https://www.instagram.com/asyed512/" />
+              {"               "}
+              <SocialIcon url="https://www.linkedin.com/in/abdur-rasheed-syed/" />
+              {"               "}
+              <SocialIcon url="https://github.com/Abdur-Rasheed/" />
+            </div>
+            <br />
+          </div>
+        </Content>
+      </Layout>
+
+      {/* </header> */}
     </div>
   );
 }
---
+++ src/Components/Resume.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import resume from "./res.pdf";
+
+class Resume extends Component {
+  render() {
+    return (
+      <div id="my-resume">
+        <a href={resume} rel="noopener noreferrer" target="_blank">
+          {" "}
+          Download
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Resume;
